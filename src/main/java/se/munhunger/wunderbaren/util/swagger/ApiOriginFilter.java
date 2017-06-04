6f7026791608d4789d02277d5081b8855a7718e5
Marcus M <marcusmunger@hotmail.com>
2017-06-04 18:09:02 +0200

    Added base java code

---
+++ src/main/java/se/munhunger/wunderbaren/util/swagger/ApiOriginFilter.java
@@ -0,0 +1,32 @@
+package se.munhunger.wunderbaren.util.swagger;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Marcus Münger on 2017-04-25.
+ */
+public class ApiOriginFilter implements javax.servlet.Filter
+{
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+						 FilterChain chain) throws IOException, ServletException
+	{
+		HttpServletResponse res = (HttpServletResponse) response;
+		res.addHeader("Access-Control-Allow-Origin", "*");
+		res.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+		res.addHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization");
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void destroy()
+	{
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException
+	{
+	}
+}
