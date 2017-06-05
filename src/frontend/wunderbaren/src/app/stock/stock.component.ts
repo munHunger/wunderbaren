464d9ec15e9c8d9e566fbbdc823eefb28d1a0d5e
Marcus M <marcusmunger@hotmail.com>
2017-06-05 20:16:37 +0200

    Checking against more than beer list

--- src/frontend/wunderbaren/src/app/stock/stock.component.ts
+++ src/frontend/wunderbaren/src/app/stock/stock.component.ts
@@ -11,25 +11,82 @@ import {Observable} from 'rxjs/Rx';
 export class StockComponent {
   beerList: Item[] = [];
   beerListHash: string;
+  ciderList: Item[] = [];
+  ciderListHash: string;
+  wineList: Item[] = [];
+  wineListHash: string;
+  drinkList: Item[] = [];
+  drinkListHash: string;
   constructor(private service: WunderbarService)
   {
-      this.service.getCategory("beer").subscribe(items => {
-        this.beerList = items.json();
-        this.beerListHash = items.headers.get('hash');
-        this.update();
-      });
+    this.service.getCategory("beer").subscribe(items => {
+      this.beerList = items.json();
+      this.beerListHash = items.headers.get('hash');
+      this.updateBeer();
+    });
+    this.service.getCategory("cider").subscribe(items => {
+      this.ciderList = items.json();
+      this.ciderListHash = items.headers.get('hash');
+      this.updateCider();
+    });
+    this.service.getCategory("wine").subscribe(items => {
+      this.wineList = items.json();
+      this.wineListHash = items.headers.get('hash');
+      this.updateWine();
+    });
+    this.service.getCategory("drink").subscribe(items => {
+      this.drinkList = items.json();
+      this.drinkListHash = items.headers.get('hash');
+      this.updateDrink();
+    });
   }
 
-  public update()
+  public updateBeer()
   {
-      this.service.getCategoryUpdate("beer", this.beerListHash).subscribe(items => {
-        var newList = items.json();
-        if(newList.length > 0)
-        {
-          this.beerList = items.json();
-          this.beerListHash = items.headers.get('hash');
-        }
-        this.update();
-      });
+    this.service.getCategoryUpdate("beer", this.beerListHash).subscribe(items => {
+      var newList = items.json();
+      if(newList.length > 0)
+      {
+        this.beerList = items.json();
+        this.beerListHash = items.headers.get('hash');
+      }
+      this.updateBeer();
+    });
+  }
+  public updateCider()
+  {
+    this.service.getCategoryUpdate("cider", this.ciderListHash).subscribe(items => {
+      var newList = items.json();
+      if(newList.length > 0)
+      {
+        this.ciderList = items.json();
+        this.ciderListHash = items.headers.get('hash');
+      }
+      this.updateCider();
+    });
+  }
+  public updateWine()
+  {
+    this.service.getCategoryUpdate("wine", this.wineListHash).subscribe(items => {
+      var newList = items.json();
+      if(newList.length > 0)
+      {
+        this.wineList = items.json();
+        this.wineListHash = items.headers.get('hash');
+      }
+      this.updateWine();
+    });
+  }
+  public updateDrink()
+  {
+    this.service.getCategoryUpdate("drink", this.drinkListHash).subscribe(items => {
+      var newList = items.json();
+      if(newList.length > 0)
+      {
+        this.drinkList = items.json();
+        this.drinkListHash = items.headers.get('hash');
+      }
+      this.updateDrink();
+    });
   }
 }
