256fb5ee4b3f7dcc80f799e58e522f6bb055d4af
Marcus M <marcusmunger@hotmail.com>
2017-06-05 19:40:51 +0200

    Fixed parts of the frontend update method

--- src/main/java/se/munhunger/wunderbaren/business/Wunderbaren.java
+++ src/main/java/se/munhunger/wunderbaren/business/Wunderbaren.java
@@ -2,10 +2,13 @@ package se.munhunger.wunderbaren.business;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import jersey.repackaged.com.google.common.util.concurrent.ThreadFactoryBuilder;
+import org.glassfish.jersey.process.JerseyProcessingUncaughtExceptionHandler;
 import org.springframework.stereotype.Component;
 import se.munhunger.wunderbaren.model.Item;
 import se.munhunger.wunderbaren.util.database.jpa.Database;
 
+import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.container.AsyncResponse;
 import javax.ws.rs.container.Suspended;
@@ -14,8 +17,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 /**
  * Created by marcu on 2017-06-04.
@@ -25,7 +27,6 @@ import java.util.concurrent.Executors;
 @Api(value = "Wunderbaren")
 public class Wunderbaren
 {
-	private static final ExecutorService TASK_EXECUTOR = Executors.newCachedThreadPool();
 
 	@POST
 	@ApiOperation(value = "Create a new item")
@@ -65,31 +66,34 @@ public class Wunderbaren
 
 	@GET
 	@ApiOperation(value = "Gets all items of the same category")
-	public void getItems(@Suspended final AsyncResponse asyncResponse, @HeaderParam("hash")int hash, @QueryParam("category") String category) throws Exception
+	public Response getItems(@QueryParam("category") String category) throws Exception
 	{
-		TASK_EXECUTOR.submit(() -> {
-			try
-			{
-				int newHash = hash;
-				int attempts = 0;
-				List items = new ArrayList<>();
-				while (newHash == hash && attempts < 300)
-				{
-					Map<String, Object> param = new HashMap<>();
-					param.put("category", category);
-					items = Database.getObjects("from Item WHERE category = :category", param);
-					newHash = getItemHash(items);
-					if (attempts > 0)
-						Thread.sleep(100);
-					attempts++;
-				}
-				asyncResponse.resume(Response.ok(items).header("hash", "" + getItemHash(items)).build());
-			}
-			catch (Exception e)
-			{
-				e.printStackTrace();
-			}
-		});
+		Map<String, Object> param = new HashMap<>();
+		param.put("category", category);
+		List items = Database.getObjects("from Item WHERE category = :category", param);
+		return Response.ok(items).header("hash", "" + getItemHash(items)).build();
+	}
+
+	@GET
+	@ApiOperation(value = "Gets all items of the same category if the hash does not equal. plain 200 OK otherwise")
+	public Response getUpdate(@HeaderParam("hash")int hash, @QueryParam("category") String category) throws Exception
+	{
+		int newHash = hash;
+		int attempts = 0;
+		List items = new ArrayList<>();
+		while (newHash == hash && attempts < 300)
+		{
+			Map<String, Object> param = new HashMap<>();
+			param.put("category", category);
+			items = Database.getObjects("from Item WHERE category = :category", param);
+			newHash = getItemHash(items);
+			if (attempts > 0)
+				Thread.sleep(100);
+			attempts++;
+		}
+		if(newHash == hash)
+			return Response.ok().build();
+		return Response.ok(items).header("hash", "" + getItemHash(items)).build();
 	}
 
 	private int getItemHash(List items)
