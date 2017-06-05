83f2b14f39e811d2120991399e341cfa82f5c3c4
Marcus Münger <marcus.munger@24money.se>
2017-06-05 09:17:51 +0200

    readded interval

--- src/frontend/wunderbaren/src/app/stock/stock.component.ts
+++ src/frontend/wunderbaren/src/app/stock/stock.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {WunderbarService} from "../wunderbar.service";
 import {Item} from "../item.model";
+import {Observable} from 'rxjs/Rx';
 
 @Component({
   selector: 'stock',
@@ -16,9 +17,10 @@ export class StockComponent {
 
   public update()
   {
-    this.service.getCategory("beer").subscribe(items => {
-      this.beerList = items;
-      setTimeout(() => { this.update(); }, 100);
+    Observable.interval(1000).subscribe(x => {
+      this.service.getCategory("beer").subscribe(items => {
+        this.beerList = items;
+      });
     });
   }
 }
