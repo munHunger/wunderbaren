f7f91ab4ea54a1f9e613b104dfb78a9e3f9407fc
munHunger <marcusmunger@hotmail.com>
2018-01-06 15:01:50 +0100

    base added

--- src/frontend/wunderbaren/src/app/app.module.ts
+++ src/frontend/wunderbaren/src/app/app.module.ts
@@ -3,29 +3,22 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { SplashComponent } from './components/splash/splash.component';
+import { CategoryComponent } from './components/category/category.component';
 import { AppComponent } from './app.component';
-import {EventComponent} from "./event/event.component";
-import {StockComponent} from "./stock/stock.component";
-import {WunderbarService} from "./wunderbar.service";
-import {ItemComponent} from "./stock/item/item.component";
-import {LoginComponent} from "./login/login.component";
-import {CookieService} from "angular2-cookie/core";
-import {UserService} from "./user.service";
 
 @NgModule({
   declarations: [
-    EventComponent,
-    StockComponent,
-    ItemComponent,
-    LoginComponent,
-    AppComponent
+    AppComponent,
+    SplashComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [WunderbarService, CookieService, UserService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/frontend/wunderbaren/src/app/event/event.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'event',
-  templateUrl: './event.component.html',
-  styleUrls: ['./event.component.css']
-})
-export class EventComponent {
-}
