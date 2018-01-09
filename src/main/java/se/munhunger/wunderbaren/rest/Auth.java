8c50ea41c8315e79508fda49135488e20d0dd51e
rikardeliasson <rikard.eliasson8@gmail.com>
2018-01-09 14:49:02 +0100

    jwt, pin and rfid working basic

--- src/main/java/se/munhunger/wunderbaren/rest/Auth.java
+++ src/main/java/se/munhunger/wunderbaren/rest/Auth.java
@@ -3,11 +3,14 @@ package se.munhunger.wunderbaren.rest;
 
 import io.swagger.annotations.*;
 import se.munhunger.wunderbaren.service.AuthService;
+import se.munhunger.wunderbaren.util.exception.UnauthorizedException;
 
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
+import java.io.UnsupportedEncodingException;
+import java.util.concurrent.ExecutionException;
 
 @Api
 @Path("/auth")
@@ -20,18 +23,24 @@ public class Auth {
     @Path("/initiate")
     @ApiOperation(value = "Setting the pin", notes = "The pin is used for user authentication")
     @ApiResponses({@ApiResponse(code = HttpServletResponse.SC_OK, message = "The pin was set")})
-    public Response initiate(@ApiParam(value = "The verification code") @QueryParam("pin") String pin) {
-        if(authservice.initiate(pin))
+    public Response initiate(@ApiParam(value = "The verification code") @QueryParam("pin") int pin) throws ExecutionException, UnauthorizedException, UnsupportedEncodingException {
+        authservice.initiate(pin);
         return Response.ok().build();
-        return Response.status(Response.Status.UNAUTHORIZED).build();
     }
 
     @POST
     @Path("/complete")
     @ApiOperation(value = "Authenticates the user", notes = "Returns a jwt token")
     @ApiResponses({@ApiResponse(code = HttpServletResponse.SC_OK, message = "Here is the jwt")})
-    public Response complete(@ApiParam(value = "The verification code") @QueryParam("pin") String pin) {
-        authservice.verifyUser(pin);
+    public Response complete(@ApiParam(value = "The verification code") @QueryParam("pin") int pin, @ApiParam(value = "The rfid") @QueryParam("rfid") String rfid) throws ExecutionException, UnsupportedEncodingException {
+        try {
+            String jwt = authservice.complete(pin, rfid);
+        }
+        catch (UnauthorizedException e) {
+            return Response.status(Response.Status.UNAUTHORIZED)
+                    .entity("message: Not authorized")
+                    .build();
+        }
         return Response.ok().build();
     }
 }
--- src/main/java/se/munhunger/wunderbaren/service/AuthService.java
+++ src/main/java/se/munhunger/wunderbaren/service/AuthService.java
@@ -1,17 +1,54 @@
 package se.munhunger.wunderbaren.service;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import se.munhunger.wunderbaren.util.exception.UnauthorizedException;
+
+import java.io.UnsupportedEncodingException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
 public class AuthService {
 
-    public boolean initiate(String pin) {
-        if (pin.equals("12345"))
-        return true;
-        return false;
+    private static int addcache(int key) {
+        System.out.println("Adding Cache");
+        return key;
     }
 
-    public boolean verifyUser(String pin) {
-        if (pin != null)
-            return true;
-        return false;
+    private static LoadingCache<Integer,String> cache = CacheBuilder.newBuilder().refreshAfterWrite(30,TimeUnit.SECONDS).
+            build(new CacheLoader<Integer, String>() {
+                @Override
+                public String load(Integer key) throws Exception {
+                    return Integer.toString(addcache(key));
+                }
+            });
+
+    public void initiate(int pin) throws UnauthorizedException, ExecutionException, UnsupportedEncodingException {
+        cache.put(pin, "none");
+        complete(pin, "proper");
     }
 
+    private static final String SECRET = "SECRET";
+
+    private static String makeToken() throws UnsupportedEncodingException {
+        String jwt = Jwts.builder()
+                .setSubject("wunder")
+                .claim("bar", "bar")
+                .signWith(SignatureAlgorithm.HS512, SECRET.getBytes("UTF-8"))
+                .compact();
+        return jwt;
+    }
+
+    public String complete(int pin, String rfid) throws ExecutionException, UnauthorizedException, UnsupportedEncodingException {
+        String check = cache.get(pin);
+        if(cache.get(pin).equals("none"))
+            cache.put(pin, rfid);
+        else if(!cache.get(pin).equals(rfid))
+            throw new UnauthorizedException();
+        return makeToken();
+    }
 }
